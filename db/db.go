b80c84c5d709d28b81e316a44eb7f5d643309ed7
Matt Ellis <mathew.ellis2@griffithuni.edu.au>
2023-07-13 18:42:57 +1000

    added basic creating and closing db connection

--- db/db.go
+++ db/db.go
@@ -2,71 +2,65 @@ package db
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-
-	"github.com/mattellis91/libretasks-server/models"
 )
 
 var Db *mongo.Database = nil
-var Connection *mongo.Client = nil
+var Client *mongo.Client = nil
 
-func InitDb() {
+func createConnection() error {
 	
 	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found")
+		panic("Could not load env")
 	}
-
-	uri := os.Getenv("MONGODB_URI")
+	
 	dbName := os.Getenv("MONGODB_DB_NAME")
-	if uri == "" {
-		log.Fatal("You must set your 'MONGODB_URL' environment variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	
+	//Client should always be nil if it gets here but just incase check if the client connection exists before making a new one
+	if Client == nil {
+		
+		uri := os.Getenv("MONGODB_URI")
+		
+		if uri == "" {
+			log.Fatal("You must set your 'MONGODB_URL' environment variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+		}
+
+		serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+		opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+
+		c, err := mongo.Connect(context.TODO(), opts)
+
+		if err != nil {
+			panic(err)
+		}
+
+		Client = c
+
 	}
+	
+	Db = Client.Database(dbName)
 
-	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-  	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
-
-	c, err := mongo.Connect(context.TODO(), opts)
-
-	if err != nil {
-		panic(err)
-	}
-
-	Connection = c
-
-	Db = c.Database(dbName)
-	coll := Db.Collection("user")
-	doc := models.User{ID: primitive.NewObjectID(), Username:  "test user 2", Email: "test2@email.com"}
-
-	res, err := coll.InsertOne(context.TODO(), doc)
-
-	if err != nil {
-		log.Fatal(err)
-		log.Fatal("Failed to insert")
-	}
-
-	fmt.Printf("Inserted document with _id: %v\n", res.InsertedID)
-
-
-	fmt.Println(Db.Name())
-	fmt.Println(coll.Name())
-
-	count, _ := coll.CountDocuments(context.TODO(), bson.M{})
-
-	fmt.Println(count)
-
+	return nil
 }
 
+func GetDatabaseConnection() *mongo.Database {
+	if Db == nil {
+		if error := createConnection(); error != nil {
+			panic("Could not connect to database")
+		}
+	}
+	return Db
+} 
+
 func CloseConnection() {
-	if Db != nil {
-		Connection.Disconnect(context.TODO())
+	if Client != nil {
+		Client.Disconnect(context.TODO())
+		Client = nil
 		Db = nil
 	}
 }
--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+
 	"github.com/mattellis91/libretasks-server/db"
+	"github.com/mattellis91/libretasks-server/models"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-func main ()  {
-	db.InitDb()	
+func main() {
 
- 	if db.Db != nil {
-		fmt.Println("Db connection already exists");
-		db.CloseConnection()
+	dbc := db.GetDatabaseConnection()
+	coll := dbc.Collection("user")
+	doc := models.User{ID: primitive.NewObjectID(), Username:  "test user 2", Email: "test2@email.com"}
+
+	res, err := coll.InsertOne(context.TODO(), doc)
+
+	if err != nil {
+		log.Fatal(err)
+		log.Fatal("Failed to insert")
 	}
+
+	fmt.Printf("Inserted document with _id: %v\n", res.InsertedID)
+
+	db.CloseConnection()
+	
 }
