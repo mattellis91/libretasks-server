2a29ff39262fd0fc5f032f092b7bc96495973c96
Matt Ellis <mathew.ellis2@griffithuni.edu.au>
2023-07-13 17:50:57 +1000

    fleshed out project structure

--- db/db.go
+++ db/db.go
@@ -1,4 +1,4 @@
-package config
+package db
 
 import (
 	"context"
@@ -8,20 +8,24 @@ import (
 
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	
+
 	"github.com/mattellis91/libretasks-server/models"
 )
 
-var Db *mongo.Database
+var Db *mongo.Database = nil
+var Connection *mongo.Client = nil
 
 func InitDb() {
+	
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found")
 	}
 
 	uri := os.Getenv("MONGODB_URI")
+	dbName := os.Getenv("MONGODB_DB_NAME")
 	if uri == "" {
 		log.Fatal("You must set your 'MONGODB_URL' environment variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
 	}
@@ -29,22 +33,17 @@ func InitDb() {
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
   	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
 
-	client, err := mongo.Connect(context.TODO(), opts)
+	c, err := mongo.Connect(context.TODO(), opts)
 
 	if err != nil {
 		panic(err)
 	}
 
-	defer func() {
-		if err := client.Disconnect(context.TODO()); err != nil {
-			panic(err)
-		}
-	}()
+	Connection = c
 
-	Db := client.Database("dev")
-	
+	Db = c.Database(dbName)
 	coll := Db.Collection("user")
-	doc := models.User{Username: "test user", Email: "test@email.com"}
+	doc := models.User{ID: primitive.NewObjectID(), Username:  "test user 2", Email: "test2@email.com"}
 
 	res, err := coll.InsertOne(context.TODO(), doc)
 
@@ -63,4 +62,11 @@ func InitDb() {
 
 	fmt.Println(count)
 
+}
+
+func CloseConnection() {
+	if Db != nil {
+		Connection.Disconnect(context.TODO())
+		Db = nil
+	}
 }
--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-	"github.com/mattellis91/libretasks-server/config"
+	"fmt"
+	"github.com/mattellis91/libretasks-server/db"
 )
 
 func main ()  {
-	config.InitDb()	
+	db.InitDb()	
+
+ 	if db.Db != nil {
+		fmt.Println("Db connection already exists");
+		db.CloseConnection()
+	}
 }
